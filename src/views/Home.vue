9db3b78141176562727228485da914a24502cb09
zt247206112 <247206112@qq.com>
2020-02-08 21:37:25 +0800

    代码提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,50 @@
   <div class="home">
     <!-- 函数式组件 -->
     <my-Function-Buttom :class="className" id="btn1">button</my-Function-Buttom>
+    <!--slot插槽-->
+    <todo-list :todos="todos">
+      <template slot-scope="slotProps">
+        <span v-if="slotProps.todo.isComplete">✓</span>
+        <span>{{slotProps.todo.text}}</span>
+      </template>
+    </todo-list>
   </div>
 </template>
 
 <script>
 import myFunctionButtom from '../components/function_buttom'
+import todoList from '../components/slot-scope/todoList'
 export default {
   name: 'home',
   components: {
-    myFunctionButtom
+    myFunctionButtom,
+    todoList
   },
   data () {
     return {
-      className: "btnname"
+      className: "btnname",
+      todos: [
+        {
+          id: 0,
+          text: 'ziwei0',
+          isComplete: false
+        },
+        {
+          text: 'ziwei1',
+          id: 1,
+          isComplete: true
+        },
+        {
+          text: 'ziwei2',
+          id: 2,
+          isComplete: false
+        },
+        {
+          text: 'ziwei3',
+          id: 3,
+          isComplete: false
+        }
+      ]
     }
   }
 }
