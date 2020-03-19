2119b86871459c0865d96e7078db66d4b06729f2
zt247206112 <247206112@qq.com>
2020-03-19 21:09:11 +0800

    多余代码剔除

--- src/components/function_buttom.js
+++ src/components/function_buttom.js
@@ -1,15 +1,7 @@
-// import Vue from 'vue'
-// Vue.component("my-function-button", {
-//   functional: true,
-//   render: function(createElement, context) {
-//     return createElement("button", context.data, context.children)
-//   }
-// })
 
 export default {
   functional: true,
   render: function(createElement, context) {
-    // console.log('ztttt', context)
     return createElement("button", context.data, context.children)
   }
 }
